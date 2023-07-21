555be9d2797374a33a91ee3abc1af41b6563f4f1
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2023-07-21 14:06:41 +0700

    implement create a lobby method

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -5,20 +5,24 @@ using Unity.Services.Core;
 using Unity.Services.Lobbies.Models;
 using Unity.Services.Lobbies;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class LobbyManager : MonoBehaviour
 {
+    public const string KEY_PLAYER_NAME = "PlayerName";
+
     public class LobbyEventArgs : EventArgs
     {
         public Lobby lobby;
     }
-
     public event EventHandler<LobbyEventArgs> OnJoinedLobby;
 
+    private string _playerName;
     private Lobby _joinedLobby;
 
     public async Task AuthenticateAndQuickJoinLobby(string playerName)
     {
+        _playerName = playerName;
         InitializationOptions initializationOptions = new InitializationOptions();
         initializationOptions.SetProfile(playerName);
         await UnityServices.InitializeAsync(initializationOptions);
@@ -29,18 +33,27 @@ public class LobbyManager : MonoBehaviour
     private async void OnSignedIn()
     {
         AuthenticationService.Instance.SignedIn -= OnSignedIn;
+        bool getNoOpenedLobbyError = false;
         try
         {
             await QuickJoinLobby();
         }
         catch (LobbyServiceException ex)
         {
-            if(ex.ErrorCode != 16006) //No opened lobbies to join error code
-            {
-                throw ex;
-            }
+            getNoOpenedLobbyError = ex.ErrorCode == 16006;
+            Debug.LogException(ex);
+        }
 
-            //TODO: create a new lobby and join
+        if(getNoOpenedLobbyError)
+        {
+            try
+            {
+                await CreateAndJoinLobby(3);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
         }
     }
 
@@ -50,5 +63,24 @@ public class LobbyManager : MonoBehaviour
         Lobby lobby = await LobbyService.Instance.QuickJoinLobbyAsync(options);
         _joinedLobby = lobby;
         OnJoinedLobby?.Invoke(this, new LobbyEventArgs { lobby = lobby });
+        Debug.Log("Quick joined a lobby");
+    }
+
+    private async Task CreateAndJoinLobby(int maxPlayers)
+    {
+        Player player = new Player(AuthenticationService.Instance.PlayerId, null, new Dictionary<string, PlayerDataObject> {
+            { KEY_PLAYER_NAME, new PlayerDataObject(PlayerDataObject.VisibilityOptions.Public, _playerName) },
+        });
+
+        CreateLobbyOptions options = new CreateLobbyOptions
+        {
+            Player = player,
+            IsPrivate = false
+        };
+
+        Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(_playerName, maxPlayers, options);
+        _joinedLobby = lobby;
+        OnJoinedLobby?.Invoke(this, new LobbyEventArgs { lobby = lobby });
+        Debug.Log("Created and joined a lobby");
     }
 }
