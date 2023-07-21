a6a7a293c0aa770ab37fea22c6090d74cc0236d3
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2023-07-21 13:15:33 +0700

    Authenticate method

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,5 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 
 public class LobbyManager : MonoBehaviour 
 {
+    public async Task Authenticate(string playerName)
+    {
+        InitializationOptions initializationOptions = new InitializationOptions();
+        initializationOptions.SetProfile(playerName);
+        await UnityServices.InitializeAsync(initializationOptions);
+        AuthenticationService.Instance.SignedIn += OnSignedIn;
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private void OnSignedIn()
+    {
+        throw new NotImplementedException();
+        //TODO: quick join lobby
+    }
 }
